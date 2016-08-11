00026500edd3b1d01445dc5bb3c7b27ffda643de
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-11 14:41:22 +0200

    Added csslint

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ var source = require('vinyl-source-stream');
 var sass = require('gulp-sass');
 var jshint = require('gulp-jshint');
 var stylish = require('jshint-stylish');
+var csslint = require('gulp-csslint');
 
 gulp.task('default', function() {
     console.log('gulp gulp gulp');
@@ -25,11 +26,13 @@ gulp.task('compile:js', ['jshint'], function() {
 gulp.task('compile:css', function() {
     gulp.src(['./src/scss/*.scss'])
         .pipe(sass())
+        .pipe(csslint())
+        .pipe(csslint.reporter())
         .pipe(gulp.dest('./public/assets/css'));
 });
 
 // This array as the 2nd parameter means the tasks I want my task to depend on
-gulp.task('watch', ['jshint','compile:js', 'compile:css'], function() {
-    gulp.watch(['./src/js/**/*.js'],['jshint','compile:js']);
+gulp.task('watch', ['compile:js', 'compile:css'], function() {
+    gulp.watch(['./src/js/**/*.js'],['compile:js']);
     gulp.watch(['./src/scss/**/*.scss'], ['compile:css']);
 });
