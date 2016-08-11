5ef2aa9d42df3e8dbdf0c5bd25bf936fc2d61a0f
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-11 11:59:39 +0200

    Using path to join the path, so it can run the same way on Windows and Linux for example

--- modules.js
+++ modules.js
@@ -1,8 +1,9 @@
 var fs = require('fs');
 var yaml = require('js-yaml');
+var path = require('path');
 
 var profile = yaml.safeLoad(fs.readFileSync('./profile.yml', 'utf8'));
 var message = profile.name + " is " + profile.age + " years old and likes ";
 message += profile.movies.join(" and ") + ".";
 
-fs.writeFileSync("./profiles/mel-jones.txt", message, "utf8");
+fs.writeFileSync(path.join(".","profiles","mel-jones.txt"), message, "utf8");
