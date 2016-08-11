c546d08542f355047a54ef2887c51d3b907b3140
Jon Ribeiro <jonathas.ribeiro@mautilus.com>
2016-08-11 11:57:41 +0200

    Added and used a yaml parser. Now also writing content to a file after reading and parsing it

--- modules.js
+++ modules.js
@@ -1,1 +1,8 @@
 var fs = require('fs');
+var yaml = require('js-yaml');
+
+var profile = yaml.safeLoad(fs.readFileSync('./profile.yml', 'utf8'));
+var message = profile.name + " is " + profile.age + " years old and likes ";
+message += profile.movies.join(" and ") + ".";
+
+fs.writeFileSync("./profiles/mel-jones.txt", message, "utf8");
